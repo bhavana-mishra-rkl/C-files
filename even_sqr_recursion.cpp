7cb84742f6b5c7d62f2190e775cec543b78981fc
Bhavana Mishra <bhavanamishrarkl@gmail.com>
2026-04-30 18:49:48 +0530

    renaming the files and reducing lines

--- even_sqr_recursion.cpp
+++ even_sqr_recursion.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
-void printSquaresReverse(int n) {
-    if (n <= 0) return;
+void EvenSqrRev(int n) {
+    if (n <= 0) return ;
     int even = 2 * n;
     printf("%d ", even * even);
-    printSquaresReverse(n - 1);
+	EvenSqrRev(n - 1);
 }
-
 int main() {
     int n;
     printf("Enter n: ");
     scanf("%d", &n);
     printf("Squares in reverse order: ");
-    printSquaresReverse(n);
-    printf("\n");
-    return 0;
+    EvenSqrRev(n);
 }
